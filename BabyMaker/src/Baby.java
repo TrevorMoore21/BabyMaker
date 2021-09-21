f9fbe08b9334f801e94e1eaf5203b0e7d836b304
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-09-21 11:28:00 -0600

    practice constuctor

---
+++ BabyMaker/src/Baby.java
@@ -0,0 +1,20 @@
+
+public class Baby
+	{
+
+		String name;
+		double weight;
+		boolean isGirl;
+		
+		public Baby(String n, double w, boolean g)
+		{
+			name = n;
+			weight = w;
+			isGirl = g;
+		}
+		
+		public static void barf()
+		{
+			System.out.println("Yucky");
+		}
+	}
---
+++ BabyMaker/src/MakingBabies.java
@@ -0,0 +1,29 @@
+
+
+public class MakingBabies
+	{
+
+		
+	
+		
+		public static void main(String[] args)
+			{
+				
+				Baby myB1 = new Baby("Leah" , 8.4, true);
+				
+				Baby myB2 = new Baby("Bob", 9.2, false);
+				
+				
+				
+				System.out.println("My first baby's name is " + myB1.name);
+				
+				if(myB1.isGirl)
+					{
+						System.out.println("She's gorgeous!");
+					}
+
+				
+				Baby.barf();
+			}
+
+	}
